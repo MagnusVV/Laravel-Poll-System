6812c0732830391b510472a91e6eb0c69074c203
MagnusVV <svartkub@msn.com>
2023-02-27 14:55:06 +0100

    Added view files
    Added view files and start page route in web.php.

---
+++ poll-app/resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<form method="post" action="/login">
+
+    @csrf
+
+    <div>
+        <label for="email">Email</label>
+        <input name="email" id="email" type="email" />
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input name="password" id="password" type="password" />
+    </div>
+    <button type="submit">Login</button>
+</form>
+
+@include('errors')
