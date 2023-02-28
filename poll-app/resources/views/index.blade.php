498329ce5a29edb923e2621a5149db700febd36b
MagnusVV <svartkub@msn.com>
2023-02-28 14:16:39 +0100

    Update index.blade.php
    Added user registration field

--- poll-app/resources/views/index.blade.php
+++ poll-app/resources/views/index.blade.php
@@ -1,3 +1,4 @@
+<h2>Login here</h2>
 <form method="post" action="/login">
 
     @csrf
@@ -11,6 +12,28 @@
         <input name="password" id="password" type="password" />
     </div>
     <button type="submit">Login</button>
+</form>
+
+@include('errors')
+
+<h2>Add new user</h2>
+<form method="post" action="/adduser">
+
+    @csrf
+
+    <div>
+        <label for="user_name">Select username</label>
+        <input name="user_name" id="user_name" type="text" />
+    </div>
+    <div>
+        <label for="email">Email</label>
+        <input name="email" id="email" type="email" />
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input name="password" id="password" type="password" />
+    </div>
+    <button type="submit">Submit</button>
 </form>
 
 @include('errors')
