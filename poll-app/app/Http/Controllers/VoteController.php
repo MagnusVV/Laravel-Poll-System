2a13ba5bf450bd1ae153caadf32a332f95b7a599
MagnusVV <svartkub@msn.com>
2023-03-09 14:31:00 +0100

    VoteController updated
    Voting now works (again).

--- poll-app/app/Http/Controllers/VoteController.php
+++ poll-app/app/Http/Controllers/VoteController.php
@@ -20,7 +20,13 @@ class VoteController extends Controller
         $this->validate($request, ['poll_id', 'email', 'vote_option_chosen']);
         $vote = $request->only('poll_id', 'email', 'vote_option_chosen');
 
-        if (Votes::where('email', '=', $vote['email'])->exists() && Votes::where('poll_id', '=', $vote['poll_id'])->exists()) {
+        $whereOptions = [
+            ['email', '=', $vote['email']],
+            ['poll_id', '=', $vote['poll_id'],]
+        ];
+
+        // if (Votes::where('email', '=', $vote['email'])->exists() && Votes::where('poll_id', '=', $vote['poll_id'])->exists()) {
+        if (Votes::where($whereOptions)->exists()) {
 
             return redirect('/')->withErrors("A vote from {$vote['email']} has already been cast on this poll. Please retype email if it's wrong, or perhaps try another poll?");
         } else {
