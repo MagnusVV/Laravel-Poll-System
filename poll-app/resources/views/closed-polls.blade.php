5c8a2b93d7d3d56c0a3cb66b9aa92435446663de
MagnusVV <svartkub@msn.com>
2023-03-09 17:59:51 +0100

    Update closed-polls.blade.php
    Vote count is now shown!

--- poll-app/resources/views/closed-polls.blade.php
+++ poll-app/resources/views/closed-polls.blade.php
@@ -9,6 +9,10 @@
         <p><b>Created By: {{$poll->user_name}}</b></p>
         <p><b>Poll Description:</b> {{$poll->poll_description}}</p>
         <p><b>Poll Closing:</b> {{$poll->date_closing}}</p>
+        <p><b>Number Of Allowed Votes: </b>{{$poll->no_of_allowed_votes}}</p>
+        <p><b>Vote Option 1: </b>{{$poll->voteOptions->first()->vote_option_1}}<br>No. of votes: {{$poll->countVotes($poll, 'vote_option_1')}}</p>
+        <p><b>Vote Option 2: </b>{{$poll->voteOptions->first()->vote_option_2}}<br>No. of votes: {{$poll->countVotes($poll, 'vote_option_2')}}</p>
+        <p>Votes total: {{$poll->votes->count()}}</p>
 
         @if ($poll->countVotes($poll, 'vote_option_1') > $poll->countVotes($poll, 'vote_option_2'))
         <p>Winner: {{$poll->voteOptions->first()->vote_option_1}}</p>
