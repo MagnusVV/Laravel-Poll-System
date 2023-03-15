51575e754dff343ca39902078735069d9b6b84e1
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-03-15 14:12:52 +0100

    Added basic styling

--- poll-app/resources/views/index.blade.php
+++ poll-app/resources/views/index.blade.php
@@ -1,12 +1,12 @@
 @include('header')
 
+<main>
     <nav>
         <a href="/active-polls">Active polls</a>
         <a href="/closed-polls">Poll results</a>
         <a href="/login-user">Login</a>
         <a href="/register-user">Register</a>
     </nav>
-<main>
 
 {{-- MV: Shows success-message from RegistrationController, if registration successful. --}}
 {{-- MV: Shows success-message from VoteController, if voting successful. --}}
