43c54941312516b07f7bf05d661fbf6596b61e31
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-03-15 13:58:00 +0100

    Added new font

--- poll-app/resources/views/header.blade.php
+++ poll-app/resources/views/header.blade.php
@@ -1,5 +1,9 @@
 <link rel="stylesheet" type="text/css" href="{{ URL::to('css/global.css') }}">
 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,400;0,700;1,200;1,400;1,700&display=swap" rel="stylesheet">
+
 <header>
     <h1>Poll system™ v13.457</h1>
 </header>
