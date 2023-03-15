9039284baf365fff3a850ba5524474a3a0f10fb0
gustavenoksson <gustav.enoksson@live.se>
2023-03-15 15:00:04 +0100

    Updated styling on dashboard

--- poll-app/resources/views/dashboard.blade.php
+++ poll-app/resources/views/dashboard.blade.php
@@ -10,38 +10,44 @@ Welcome {{$user->user_name}}!</p>
 <a href="/dashboard-completed-polls">View Completed Polls</a>
 
 <main>
-<form method="post" action="/polls" class="create_poll_form">
+<section class="create_poll_section">
+    <form method="post" action="/polls" class="create_poll_form">
 
-    @csrf
+        <h2>Create New Poll</h2>
 
-    <div>
-        <label for="poll_title">Poll Title</label>
-        <input type="text" name="poll_title" required>
-    </div>
-    <div>
-        <label for="poll_description">Poll Description</label>
-        <input type="text" name="poll_description" required>
-    </div>
-    <div>
-        <label for="date_closing">Poll Closing Date</label>
-        <input type="date" name="date_closing" min="<?= date("Y-m-d") ?>" required>
-    </div>
-    <div>
-        <label for="no_of_allowed_votes">Number Of Allowed Votes</label>
-        <input type="number" name="no_of_allowed_votes" required>
-    </div>
-    <div>
-        <label for="vote_option_1">Vote Option 1</label>
-        <input type="text" name="vote_option_1" required>
-    </div>
-    <div>
-        <label for="vote_option_2">Vote Option 2</label>
-        <input type="text" name="vote_option_2" required>
-    </div>
-    <button type="submit">Add Poll</button>
-</form>
+        @csrf
+
+        <div>
+            <label for="poll_title">Poll Title</label>
+            <input type="text" name="poll_title" required>
+        </div>
+        <div>
+            <label for="poll_description">Poll Description</label>
+            <input type="text" name="poll_description" required>
+        </div>
+        <div>
+            <label for="date_closing">Poll Closing Date</label>
+            <input type="date" name="date_closing" min="<?= date("Y-m-d") ?>" required>
+        </div>
+        <div>
+            <label for="no_of_allowed_votes">Number Of Allowed Votes</label>
+            <input type="number" name="no_of_allowed_votes" required>
+        </div>
+        <div>
+            <label for="vote_option_1">Vote Option 1</label>
+            <input type="text" name="vote_option_1" required>
+        </div>
+        <div>
+            <label for="vote_option_2">Vote Option 2</label>
+            <input type="text" name="vote_option_2" required>
+        </div>
+        <button type="submit">Add Poll</button>
+    </form>
+</section>
 
 <section class="poll_section">
+    <h2>Active Polls</h2>
+    <div class="poll_cards_container">
     @foreach ($user->polls as $poll)
         @if (!$poll->poll_closed)
         <article class="poll_card">
@@ -67,8 +73,10 @@ Welcome {{$user->user_name}}!</p>
         </article>
         @endif
     @endforeach
-</main>
-<footer></footer>
+    </div>
 
-@include('errors')
+    @include('errors')
+</main>
+
+@include('footer')
 
